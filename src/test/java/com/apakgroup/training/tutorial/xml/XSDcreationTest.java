909dc2a550b059b11666128637ca4111e67eb26a
George Baras <george.baras@sword-apak.com>
2016-05-06 11:36:09 +0100

    made modifications to carValuation page, added test to XSDcreationTest

--- src/test/java/com/apakgroup/training/tutorial/xml/XSDcreationTest.java
+++ src/test/java/com/apakgroup/training/tutorial/xml/XSDcreationTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 import com.apakgroup.training.tutorial.model.PriceBandImpl;
 import com.apakgroup.training.tutorial.model.PriceRecordImpl;
+import com.apakgroup.training.tutorial.model.PriceRecordList;
+import com.apakgroup.training.tutorial.model.Vehicle;
 
 public class XSDcreationTest {
 
@@ -22,6 +24,18 @@ public class XSDcreationTest {
     public final void testCreatePriceBandXSD() throws JAXBException, IOException {
         File file = new File("priceBandSchema");
         XSDcreation.writeXsd(file, PriceBandImpl.class);
+    }
+
+    @Test
+    public final void testCreatePriceRecordListXSD() throws JAXBException, IOException {
+        File file = new File("priceRecordListSchema");
+        XSDcreation.writeXsd(file, PriceRecordList.class);
+    }
+
+    @Test
+    public final void testCreateVehicleXSD() throws JAXBException, IOException {
+        File file = new File("vehicleSchema");
+        XSDcreation.writeXsd(file, Vehicle.class);
     }
 
 }
